2298a75e76cde739e51e1d6a046ebea058fc8ee6
odoh collins <odohcollins6@gmail.com>
2023-12-03 23:50:48 +0100

    add all fil

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import PlayerList from './components/PlayerList'
+
+function App() {
+ 
+
+  return (
+    <>
+    <br></br>
+    <div className='container'>
+      <h1>React Props</h1>
+    </div>
+      
+      <PlayerList />
+      
+    </>
+  )
+}
+
+export default App
